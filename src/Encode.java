ff0a662f912488ba811235f541b62098412e09a5
onysh <onysh@192.168.0.118>
2019-06-13 15:40:07 +0900

    initial push

---
+++ src/Encode.java
@@ -0,0 +1,62 @@
+
+public class Encode {
+	static int[] ip =   {58, 50, 42, 34, 26, 18, 10 , 2,
+						 60, 52, 44, 36, 28, 20, 12,  4,
+						 62, 54, 46, 38, 30, 22, 14,  6,
+						 64, 56, 48, 40, 32, 24, 16,  8,
+						 57, 49, 41, 33, 25, 17,  9,  1,
+						 59, 51, 43, 35, 27, 19, 11,  3,
+						 61, 53, 45, 37, 29, 21, 13,  5,
+						 63, 55, 47, 39, 31, 23, 15,  7};
+	
+	static int[] ipi =	{40,  8, 48, 16, 56, 24, 64, 32,
+						 39,  7, 47, 15, 55, 23, 63, 31,
+						 38,  6, 46, 14, 54, 22, 62, 30,
+						 37,  5, 45, 13, 53, 21, 61, 29,
+						 36,  4, 44, 12, 52, 20, 60, 28,
+						 35,  3, 43 ,11, 51, 19, 59, 27,
+						 34,  2, 42, 10, 50, 18, 58, 26,
+						 33,  1, 41,  9, 49, 17, 57, 25};
+	
+	static String chiper="";
+
+
+
+	public static String Crypto(String input, long[] subkey) {
+
+		Feistel feistel= new Feistel();
+		String afterip = "";
+		
+		for(int i=0;i<64;i++)
+		{
+			afterip=afterip+input.charAt(ip[i]-1);
+		}
+		String afterip_1_str = String.format("%64s", afterip.substring(0,32) ).replace(' ', '0');
+		
+		String afterip_2_str = String.format("%64s", afterip.substring(32,64) ).replace(' ', '0');
+		
+		int afterip_1=(int)(Long.parseLong(afterip_1_str,2)&0xffffffff);
+		int afterip_2=(int)(Long.parseLong(afterip_2_str,2)&0xffffffff);
+		
+		int tmp, afterfiestal;
+		
+		for(int i=0; i<16; i++)
+		{
+			
+			afterfiestal = feistel.dofeistel(afterip_2, subkey[i]);
+			tmp = afterip_2;
+			afterip_2 = afterfiestal ^ afterip_1;
+			afterip_1 = tmp;
+		}
+
+		long result=((long)afterip_2<<32) | afterip_1;
+		String resultstr=String.format("%64s", Long.toBinaryString(result) ).replace(' ','0');
+	
+		for(int i=0; i<64;i++)
+		{
+			chiper=chiper + resultstr.charAt(ipi[i]-1);
+		}
+		
+		return chiper;
+	}
+}
---
+++ src/Key.java
@@ -0,0 +1,68 @@
+
+public class Key {
+	static private int[] pc1=
+		        {57,49,41,33,25,17, 9,
+	              1,58,50,42,34,26,18,
+			     10, 2,59,51,43,35,27,
+			     19,11, 3,60,52,44,36,
+			     63,55,47,39,34,23,15,
+		      	  7,62,54,46,38,30,22,
+		         14, 6,61,53,45,37,29,
+		         21,13, 5,28,20,12, 4};
+
+	static private int[] pc2= 
+		        {14,17,11,24, 1, 5,
+			 	  3,28,15, 6,21,10,
+			 	 23,19,12, 4,26, 8,
+			 	 16, 7,27,20,13, 2,
+			 	 41,52,31,37,47,55,
+			 	 30,40,51,45,33,48,
+			 	 44,49,39,56,34,53,
+			 	 46,42,50,36,29,32};
+	
+	static int[] shift = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
+	public static int rotateLeft(int target, int amount)
+	{
+		int ltarget=target;
+		int lamount=amount;
+		while(lamount!=0)
+		{
+			ltarget=ltarget<<1;
+			if((ltarget&0xf0000000)!=0)
+			{
+				ltarget|=1;
+				ltarget=ltarget&0xfffffff;
+			}
+			lamount--;
+		}
+		return ltarget;
+	}
+	
+	public static long[] MakeSubKey(long key) {
+		String keystr = String.format("%64s", Long.toBinaryString(key)).replace(' ', '0');
+		long[] subkey=new long[16];
+		String afterpc1="";
+		
+		for(int i=0;i<pc1.length;i++)
+		{
+			afterpc1= afterpc1 + keystr.charAt(pc1[i]-1);
+		}
+		int beforepc2_1=Integer.parseInt(afterpc1.substring(0,28),2);
+		int beforepc2_2=Integer.parseInt(afterpc1.substring(28,56),2);
+
+		for(int i=0; i<16;i++)
+		{
+			beforepc2_1=rotateLeft(beforepc2_1, shift[i]);
+			beforepc2_2=rotateLeft(beforepc2_2, shift[i]);
+			subkey[i]= ((long)beforepc2_1<<28) | beforepc2_2;
+			String afterpc2="";
+			String test = String.format("%56s", Long.toBinaryString(subkey[i])).replace(' ', '0');
+			for(int j=0; j<48;j++)
+			{
+				afterpc2=afterpc2+test.charAt(pc2[j]-1);
+			}
+			subkey[i]=Long.parseLong(afterpc2,2);
+		}
+		return subkey;
+	}
+}
